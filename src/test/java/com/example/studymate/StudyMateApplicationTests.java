345d317f1a53b1a2eabcc3977175430286caa5a3
6zeropk <nemm3623@naver.com>
2025-06-29 13:56:14 +0900

    feat : 초기 프로젝트 생성

---
+++ src/test/java/com/example/studymate/StudyMateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.studymate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyMateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
