d3c47e5239a8b4427ab12f5038e55dab470fd7de
6zeropk <nemm3623@naver.com>
2025-06-30 15:51:35 +0900

    refactor: UserDto 요청/응답으로 분리

--- src/main/java/com/example/studymate/dto/UserRequestDto.java
+++ src/main/java/com/example/studymate/dto/UserRequestDto.java
@@ -6,11 +6,12 @@ import lombok.*;
 @Getter @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-public class UserDto {
+public class UserRequestDto {
 
     // User 객체에 직접 접근하는 것을 막기 위해 사용
 
     private String username;
-    private String password;
     private String email;
+    private String password;
+    private String nickname;
 }
